b0b76254c03487c3b6fea317654d205d95b5f17b
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-12-26 12:13:09 -0300

    criando um log de maneira de objeto JSON, usei os events, o fs para gravar os arquivos e o path para pegar o caminho de log.

--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
-const os = require('os');
+const os = require('os')
+const log = require('./logger')
 
 const { freemem, totalmem } = os;
 
@@ -19,6 +20,8 @@ setInterval(() => {
     console.clear();
     console.log('======= PC STATS =======');
     console.table(stats);
+
+    log(`${JSON.stringify(stats)}\n`);
 
 }, 1000);
 
